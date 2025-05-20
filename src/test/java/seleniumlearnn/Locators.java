1190369d2e87117067ebf769231329e38cf46de1
Gupta <roshigupta@deloitte.com>
2025-05-20 16:12:01 +0530

    Initial commit - Locators

---
+++ src/test/java/seleniumlearnn/Locators.java
@@ -0,0 +1,45 @@
+package seleniumlearnn;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Locators {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./chrome.exe");
+		WebDriver driver = new ChromeDriver();
+		// implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		// url
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+
+		// starts
+		driver.findElement(By.id("inputUsername")).sendKeys("rahul");
+		driver.findElement(By.name("inputPassword")).sendKeys("hello123");
+		driver.findElement(By.className("signInBtn")).click();
+		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Roshini");
+		driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("roshini@sa.com");
+		driver.findElement(By.xpath("//input[@type='text'][2]")).clear();
+		driver.findElement(By.cssSelector("input[type='text']:nth-child(3)")).sendKeys("roshini@gmail.com");
+		driver.findElement(By.xpath("//form/input[3]")).sendKeys("1234567890");
+//		css selector class .
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		System.out.println(driver.findElement(By.cssSelector("form p")).getText());
+		driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+		Thread.sleep(1000);
+//		css selector id #
+		driver.findElement(By.cssSelector("#inputUsername")).sendKeys("rahul");
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.id("chkboxOne")).click();
+		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+
+	}
+}
